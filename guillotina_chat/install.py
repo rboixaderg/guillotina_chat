355a2cfe99ae0393891055aeabc4d708f42d60b5
Roger Boixader Güell <rboixaderg@gmail.com>
2023-01-18 22:40:29 +0100

    feat: addon

--- guillotina_chat/install.py
+++ guillotina_chat/install.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from guillotina import configure
 from guillotina.addons import Addon
+from guillotina.content import create_content_in_container
+from guillotina.interfaces import IRolePermissionManager
 from guillotina.utils import get_registry
 
 
@@ -11,8 +13,20 @@ class ManageAddon(Addon):
 
     @classmethod
     async def install(cls, container, request):
-        registry = await get_registry(container)  # noqa
-        # install logic here...
+        roleperm = IRolePermissionManager(container)
+        roleperm.grant_permission_to_role_no_inherit(
+            'guillotina.AccessContent', 'guillotina.Member')
+
+        if not await container.async_contains('conversations'):
+            conversations = await create_content_in_container(
+                container, 'Folder', 'conversations',
+                id='conversations', creators=('root',),
+                contributors=('root',))
+            roleperm = IRolePermissionManager(conversations)
+            roleperm.grant_permission_to_role(
+                'guillotina.AddContent', 'guillotina.Member')
+            roleperm.grant_permission_to_role(
+                'guillotina.AccessContent', 'guillotina.Member')
 
     @classmethod
     async def uninstall(cls, container, request):
